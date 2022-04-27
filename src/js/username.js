51bff6f8ef352b15183b58708a77121920186e62
Jesus Herrera <heisusin@gmail.com>
2022-04-27 10:38:16 +0200

    user can enter the game

--- src/js/username.js
+++ src/js/username.js
@@ -1,13 +1,20 @@
 const userName = document.getElementById('username');
-const userButton = document.getElementById('btnLogin');
+const startButton = document.getElementById('btn-start');
+const welcomeUser = document.getElementById('welcome');
+const gameKeyboard = document.getElementById('keyboard');
+const gameBoard = document.getElementById('letter-board');
 
-console.log(userButton);
-userButton.addEventListener('click', userLog);
+startButton.addEventListener('click', userLog);
 
 function userLog(e) {
     e.preventDefault();
     let userIn = userName.value;
-    localStorage.setItem('user', userIn);
+    if (userIn === '') {
+        console.log('vacio');
+    }else {
+        localStorage.setItem('user', userIn);
+        welcomeUser.classList.replace('active', 'inactive');
+        gameKeyboard.classList.replace('inactive', 'active');
+        gameBoard.classList.replace('inactive', 'active');
+    }
 }
-
-// export {userLog}
