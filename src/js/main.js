c5da0ff400e6b355796a8f6e3f973bb20e36a8de
Julio Macias Caldera <thebigjyt@gmail.com>
2022-04-28 09:02:05 +0200

    vuelta a lo que había

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ const circle = document.querySelectorAll('[data-circle]');
 
 const sendValues = document.querySelector('#send');
 const deleteLetter = document.querySelector('#back');
+// let userGuessWord = null;
 
 //Global Variables
 
@@ -38,23 +39,26 @@ const getValueButton = () => {
     keyButton.forEach(key => {
 
         key.addEventListener('click', () => {
-
-            arrayWord.push(key.textContent);
+            
 
             if (circle[index].innerText === "") {
 
                 circle[index].textContent += key.textContent.toUpperCase();
-
+                
                 index++;
             } else {
                 index++;
             }
-
+            // userGuessWord = arrayWord.join('')
+            
         })
 
-
+        // console.log(newArray);
+            
 
     })
+
+    
 
 }
 
@@ -103,19 +107,45 @@ const changeColor = () => {
    
     const localString = localStorage.getItem("solution").toLowerCase();
     const splitWord = localString.split('');
+    // const comparedObj = {
+    //     computerWord: localString,
+    //     guessWord: userGuessWord
+    // }
+    // const {computerWord , guessWord} = comparedObj
+ 
+    // for (const userLetter of guessWord) {
 
+
+    //     if (computerWord.includes(userLetter) == false) {
+    //         console.log(userLetter, 'this letter is not in the word')
+    //     } else {
+    //         if (guessWord.indexOf(userLetter) == computerWord.indexOf(userLetter)) {
+    //             console.log(userLetter, 'this letter is in this word and in the correct place')
+    //         } else if (guessWord == computerWord) {
+    //             console.log('You won - that is the exact word')
+    //         } else {
+    //             console.log('this letter exist but not in the same position')
+    //         }
+    //     }
+    // }
+   
+    
     mixArray.push(splitWord);
-
+    
     mixArray.forEach(word =>{
+
         word.forEach((letter, letterIndex) => {
-            
+            console.log(letter)
             if(mixArray[0][letterIndex] === mixArray[1][letterIndex]){
-                
+
                 circle[letterIndex].style.backgroundColor = "green";
                 
             }
+            
         })
     })
+
+    console.log(mixArray)
 
 }
 
@@ -132,10 +162,10 @@ const sendValuesFunction = () => {
     sendValues.addEventListener('click', () => {
         const arrayString = arrayWord.join('');
         changeColor();
-        arrayWord = [];
+        // arrayWord = [];
         guessWord(arrayString);
 
-
+        console.log(arrayWord);
 
     })
 }
